76f3b2879efaa9bfe552c255d0670bb752b762d2
Gabriel <gabrielmacedo.dev@gmail.com>
2017-11-23 01:21:09 -0300

    Upando os arquivos do front-end

---
+++ app.js
@@ -0,0 +1,67 @@
+function mostrarRelogio() {
+	
+	var presente = new Date;
+	
+	var horaMinuto = presente.toString().substr(16, 5);
+	var segundos = presente.toString().substr(22, 2);
+
+	document.getElementById('hora-minuto').innerHTML = horaMinuto + ':';
+	document.getElementById('segundos').innerHTML = segundos;
+	// document.getElementById('segundos').style.transition = "all 2s";
+}
+
+
+function mostrarData(){
+	
+	const meses = {
+		0: '01',
+		1: '02',
+		2: '03',
+		3: '04',
+		4: '05',
+		5: '06',
+		6: '07',
+		7: '08',
+		8: '09',
+		9: '10',
+		10: '11',
+		11: '12'
+	}
+	
+	var presente = new Date;
+
+	var mes = presente.getMonth();
+	var mesPresente = meses[mes];
+
+	var dia = presente.toString().substr(8, 2);
+	var ano = presente.toString().substr(13, 2);
+	
+	document.getElementById('dia-mes').innerHTML = dia + '/' + mesPresente;
+	document.getElementById('ano').innerHTML = ano;
+}
+
+function diaOuNoite(){
+	
+	var presente = new Date;
+
+	var hora = presente.getHours();
+
+	if ((hora >= 6) && (hora < 12)) { 
+		document.getElementById('saudacao-msg').innerHTML = 'Bom dia!';
+	} else if ((hora >= 12) && (hora < 18)) { 
+		document.getElementById('saudacao-msg').innerHTML = 'Boa tarde!';
+	} else {
+		document.getElementById('saudacao-msg').innerHTML = 'Boa noite!';
+		document.getElementById('icone-tempo').src = 'img/lua.png';
+	}
+}
+
+setInterval(mostrarRelogio, 1000);
+mostrarData();
+diaOuNoite();
+
+
+
+
+
+
