322c0e319f9564095071d1ed4ff1857c920b2367
Gabriel <gabrielmacedo.dev@gmail.com>
2017-12-28 16:48:34 -0300

    Organização do código

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-const hbs = require('hbs');
 
 var {mongoose} = require('../db/mongoose'); 
 
-var funcoesGerais = require('../funcoes-gerais');
 var usuario_controle = require('../controllers/usuario-controle');
 var folha_controle = require('../controllers/folha-controle');
 
+
+router.get('/', (req, res) => {
+  res.render('capa', { 
+  	tituloPagina: 'Capa'});
+});
+
+router.get('/saiba-mais', (req, res) => {
+  res.render('saiba-mais', { 
+  	tituloPagina: 'Saiba Mais'});
+});
 
 router.post('/', usuario_controle.loginUsuario_post);
 router.post('/juntarse', usuario_controle.novoUsuario_post);
@@ -17,31 +25,6 @@ router.get('/diario/:idUsuario/exibir-folhas', folha_controle.folhasUsuario);
 router.get('/diario/:idUsuario/exibir-folhas/:numeroFolha', folha_controle.mostrarFolha_get); 
 router.get('/diario/:idUsuario/nova-folha', folha_controle.novaFolha_get);
 router.post('/diario/:idUsuario/nova-folha', folha_controle.novaFolha_post); 
-
-
-/* GET Capa. */
-router.get('/', (req, res, next) => {
-  res.render('capa', { 
-  	tituloPagina: 'Capa'});
-});
-
-
-/* GET Saiba Mais. */
-router.get('/saiba-mais', (req, res, next) => {
-  res.render('saiba-mais', { 
-  	tituloPagina: 'Saiba Mais'});
-});
-
-
-
-// router.get('/rascunhos', (req, res)=>{
-
-// var dataEscrita = funcoesGerais.coletarDataCompleta();
-
-// res.render('rascunhos', {dataEscrita: dataEscrita});
-
-// });
-
 
 
 module.exports = router;
